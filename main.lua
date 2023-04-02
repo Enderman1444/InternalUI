2b202184b35702582e8528d2038051001b058f3a
heckur <76869975+Enderman1444@users.noreply.github.com>
2023-04-02 10:08:16 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,16 @@
+local internal = game:GetObjects("rbxassetid://12974664668")[1]
+local frame = internal.Frame
+
+frame.Clr.MouseButton1Down:Connect(function()
+	local Source = frame.Source
+	Source.Text = ""
+end)
+
+frame.Exe.MouseButton1Down:Connect(function()
+	local Source = frame.Source
+	loadstring(Source.Text)()
+end)
+
+frame.Exit.MouseButton1Down:Connect(function()
+	frame:Destroy()
+end)
